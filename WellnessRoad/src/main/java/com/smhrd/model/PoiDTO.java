6df32bd4246c37bb44af4f7558dc9f2bc9f80db6
User <User@DESKTOP-Q81HQFE>
2024-03-26 11:22:41 +0900

    poi변경

--- WellnessRoad/src/main/java/com/smhrd/model/PoiDTO.java
+++ WellnessRoad/src/main/java/com/smhrd/model/PoiDTO.java
@@ -1,133 +1,145 @@
 package com.smhrd.model;
 
 public class PoiDTO {
-	private int idx;
-	private String name;
-	private String addr;
-	private double lat;
-	private double lng;
-	private String info;
-	private String closeDay;
-	private String available;
-	private String parking;
-	private String tag;
-	private String imgUrl;
+	private int poi_idx;
+	private String poi_name;
+	private String poi_addr;
+	private double poi_lat;
+	private double poi_lng;
+	private String poi_info;
+	private String poi_closeDay;
+	private String poi_available;
+	private String poi_parking;
+	private String poi_region;
+	private String poi_tag;
+	private String poi_img;
 	
-	public PoiDTO(int idx, String name, String addr, double lat, double lng, String info, String closeDay,
-			String available, String parking, String tag, String imgUrl) {		
-		this.idx = idx;
-		this.name = name;
-		this.addr = addr;
-		this.lat = lat;
-		this.lng = lng;
-		this.info = info;
-		this.closeDay = closeDay;
-		this.available = available;
-		this.parking = parking;
-		this.tag = tag;
-		this.imgUrl = imgUrl;
-	}	
+	public PoiDTO(int poi_idx, String poi_name, String poi_addr, double poi_lat, double poi_lng, String poi_info,
+			String poi_closeDay, String poi_available, String poi_parking, String poi_region, String poi_tag,
+			String poi_img) {
+		this.poi_idx = poi_idx;
+		this.poi_name = poi_name;
+		this.poi_addr = poi_addr;
+		this.poi_lat = poi_lat;
+		this.poi_lng = poi_lng;
+		this.poi_info = poi_info;
+		this.poi_closeDay = poi_closeDay;
+		this.poi_available = poi_available;
+		this.poi_parking = poi_parking;
+		this.poi_region = poi_region;
+		this.poi_tag = poi_tag;
+		this.poi_img = poi_img;
+	}
 	
 	public PoiDTO() {
 		
 	}
+
+	public int getPoi_idx() {
+		return poi_idx;
+	}
+
+	public String getPoi_name() {
+		return poi_name;
+	}
+
+	public String getPoi_addr() {
+		return poi_addr;
+	}
+
+	public double getPoi_lat() {
+		return poi_lat;
+	}
+
+	public double getPoi_lng() {
+		return poi_lng;
+	}
+
+	public String getPoi_info() {
+		return poi_info;
+	}
+
+	public String getPoi_closeDay() {
+		return poi_closeDay;
+	}
+
+	public String getPoi_available() {
+		return poi_available;
+	}
+
+	public String getPoi_parking() {
+		return poi_parking;
+	}
+
+	public String getPoi_region() {
+		return poi_region;
+	}
+
+	public String getPoi_tag() {
+		return poi_tag;
+	}
+
+	public String getPoi_img() {
+		return poi_img;
+	}
+
+	public void setPoi_idx(int poi_idx) {
+		this.poi_idx = poi_idx;
+	}
+
+	public void setPoi_name(String poi_name) {
+		this.poi_name = poi_name;
+	}
+
+	public void setPoi_addr(String poi_addr) {
+		this.poi_addr = poi_addr;
+	}
+
+	public void setPoi_lat(double poi_lat) {
+		this.poi_lat = poi_lat;
+	}
+
+	public void setPoi_lng(double poi_lng) {
+		this.poi_lng = poi_lng;
+	}
+
+	public void setPoi_info(String poi_info) {
+		this.poi_info = poi_info;
+	}
+
+	public void setPoi_closeDay(String poi_closeDay) {
+		this.poi_closeDay = poi_closeDay;
+	}
+
+	public void setPoi_available(String poi_available) {
+		this.poi_available = poi_available;
+	}
+
+	public void setPoi_parking(String poi_parking) {
+		this.poi_parking = poi_parking;
+	}
+
+	public void setPoi_region(String poi_region) {
+		this.poi_region = poi_region;
+	}
+
+	public void setPoi_tag(String poi_tag) {
+		this.poi_tag = poi_tag;
+	}
+
+	public void setPoi_img(String poi_img) {
+		this.poi_img = poi_img;
+	}
 	
 	
-	public int getIdx() {
-		return idx;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getAddr() {
-		return addr;
-	}
-
-	public double getLat() {
-		return lat;
-	}
-
-	public double getLng() {
-		return lng;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	public String getCloseDay() {
-		return closeDay;
-	}
-
-	public String getAvailable() {
-		return available;
-	}
-
-	public String getParking() {
-		return parking;
-	}
-
-	public String getTag() {
-		return tag;
-	}
-
-	public String getImgUrl() {
-		return imgUrl;
-	}
 	
 	
-	
-
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setAddr(String addr) {
-		this.addr = addr;
-	}
-
-	public void setLat(double lat) {
-		this.lat = lat;
-	}
-
-	public void setLng(double lng) {
-		this.lng = lng;
-	}
-
-	public void setInfo(String info) {
-		this.info = info;
-	}
-
-	public void setCloseDay(String closeDay) {
-		this.closeDay = closeDay;
-	}
-
-	public void setAvailable(String available) {
-		this.available = available;
-	}
-
-	public void setParking(String parking) {
-		this.parking = parking;
-	}
-
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
-
-	public void setImgUrl(String imgUrl) {
-		this.imgUrl = imgUrl;
-	}
 	
 	
 	
 	
 	
 }
+
 
 
